7bb6dc5c4bd71599c1380039dd7173df238293f2
Jack Bevis <jack.bevis@gmail.com>
2017-07-28 01:03:21 -0600

    finished client side code. MVP on functionality and UI

--- client/index.js
+++ client/index.js
@@ -1,4 +1,8 @@
-
+$(document).ready(() => {
+	getInventory();
+	persistCartItems();
+	getOrderHistory();
+})
 	
 const getInventory = () => {
 	fetch('/api/v1/inventory')
@@ -24,8 +28,6 @@ const appendItem = (array) => {
 	})
 };
   
-getInventory()
-
 const addToCart = (item) => {
 	$('#cart-card-holder').append(
 		`<article class="cart-card">
@@ -41,8 +43,105 @@ const sumTotal = (price) => {
 	let newTotal = currentTotalInt + price;
 
 	$('#total').text(`$${newTotal}`)
-
 };
+
+const persistCartItems = () => {
+  if (!localStorage.getItem('cartItems')) {
+    return;
+  }
+
+  const currentCart = JSON.parse(localStorage.getItem('cartItems'));
+
+  currentCart.forEach((item) => {
+  	let price = parseInt(item.price.split('').slice(1).join(''));
+  	console.log(price)
+    
+    addToCart(item);
+    sumTotal(price);
+  });
+
+	const savedTotal = currentCart.reduce((acc, item) => {
+		
+		let itemPrice = parseInt(item.price.split('').slice(1).join(''));
+		
+		return acc + itemPrice
+	}, 0);
+  
+  $('#total').text(`$${savedTotal}`)
+};
+
+const setLocalStorage = (obj) => {
+  if (!localStorage.getItem('cartItems')) {
+    let stringArray = JSON.stringify([]);
+
+    localStorage.setItem('cartItems', stringArray);
+  }
+  let cartItems = JSON.parse(localStorage.getItem('cartItems'));
+
+  cartItems.push(obj);
+
+  let stringcartItems = JSON.stringify(cartItems);
+
+  localStorage.setItem('cartItems', stringcartItems);
+};
+
+const processOrder = (total) => {
+	fetch('/api/v1/orderHistory', {
+		method: 'POST',
+		headers: { 'Content-Type':'application/json' },
+		body: JSON.stringify({ total: total })
+	})
+	.then(() => {
+		getOrderHistory()
+	});
+};
+
+const getOrderHistory = () => {
+	fetch('/api/v1/orderHistory')
+	.then(response => response.json())
+	.then(orders => {
+		prependOrders(orders)
+	});
+};
+
+const prependOrders = (array) => {
+	if (!array.length) {
+		return;
+	}
+	$('#order-card-holder').empty();
+	array.forEach(order => {
+		let cardTotal = `$${order.total}`;
+		let cardDate = order.created_at.slice(0, 10);
+
+		$('#order-card-holder').prepend(
+			`<article class="cart-card">
+				<p class="order-total">Order total: ${cardTotal}</p>
+				<p class="order-date">Order date: ${cardDate}</p>
+			</article>`
+		);
+	});
+};
+
+const clearCart = () => {
+	localStorage.clear();
+	$('#cart-card-holder').empty();
+	$('#total').text('$0.00');
+}
+
+$('#toggle-order-history').on('click', function() {
+	const orderExpand = $('#toggle-order-history');
+
+	$('#orders').toggleClass('hidden');
+	!orderExpand.text('+') ? orderExpand.text('+') : orderExpand.text('-');
+});
+
+$('#toggle-cart').on('click', function() {
+	const orderExpand = $('#toggle-cart');
+
+	$('#cart-list').toggleClass('hidden');
+	!orderExpand.text('+') ? orderExpand.text('+') : orderExpand.text('-');
+});
+
 
 
 $('.inventory-gallery').on('click', '.add-to-cart', function() {
@@ -53,5 +152,12 @@ $('.inventory-gallery').on('click', '.add-to-cart', function() {
 
 	addToCart(cartItem);
 	sumTotal(priceInt);
+	setLocalStorage(cartItem);
+});
 
-})
+$('.checkout-btn').on('click', function() {
+	let orderTotal = parseInt($('#total').text().slice(1));
+	
+	processOrder(orderTotal);
+	clearCart();
+});
