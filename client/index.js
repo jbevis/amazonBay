9f15db64946b5bdf730ed76525992a91395f45ec
Jack Bevis <jack.bevis@gmail.com>
2017-07-31 21:42:17 -0600

    removed hover from aside lable, and added client side error handling

--- client/index.js
+++ client/index.js
@@ -1,48 +1,49 @@
 $(document).ready(() => {
-	getInventory();
-	persistCartItems();
-	getOrderHistory();
+  getInventory();
+  persistCartItems();
+  getOrderHistory();
 })
-	
+  
 const getInventory = () => {
-	fetch('/api/v1/inventory')
-	.then(response => response.json())
+  fetch('/api/v1/inventory')
+  .then(response => response.json())
   .then(inventory => {
-   	appendItem(inventory)
+    appendItem(inventory)
   })
+  .catch((error) => {
+    console.log(error, 'There was an error retrieving the inventory.')
+  });
 };
 
 const appendItem = (array) => {
-	console.log(array)
-	array.forEach(item => {
-
-	  $('.inventory-gallery').append(
-	    `<article class="inventory-item">
-	      <h4 class="item-title">${item.title}</h4>
-	      <p class="item-description">${item.description}</p>
-	      <img class="item-image" alt="${item.title}" src="${item.image}"/>
-	      <p class="item-price">$${item.price}</p>
-	      <button class="add-to-cart">Add to Cart</button>
-	    </article>`
-	  )
-	})
+  array.forEach(item => {
+    $('.inventory-gallery').append(
+      `<article class="inventory-item">
+        <h4 class="item-title">${item.title}</h4>
+        <p class="item-description">${item.description}</p>
+        <img class="item-image" alt="${item.title}" src="${item.image}"/>
+        <p class="item-price">$${item.price}</p>
+        <button class="add-to-cart">Add to Cart</button>
+      </article>`
+    )
+  })
 };
   
 const addToCart = (item) => {
-	$('#cart-card-holder').append(
-		`<article class="cart-card">
-			<p>Item: ${item.title}</p>
-			<p>Price: ${item.price}</p>
-		</article>`
-	)
+  $('#cart-card-holder').append(
+    `<article class="cart-card">
+      <p>Item: ${item.title}</p>
+      <p>Price: ${item.price}</p>
+    </article>`
+  )
 };
 
 const sumTotal = (price) => {
-	let currentTotal = $('#total').text().split('').slice(1).join('');
-	let currentTotalInt = parseInt(currentTotal);
-	let newTotal = currentTotalInt + price;
+  let currentTotal = $('#total').text().split('').slice(1).join('');
+  let currentTotalInt = parseInt(currentTotal);
+  let newTotal = currentTotalInt + price;
 
-	$('#total').text(`$${newTotal}`)
+  $('#total').text(`$${newTotal}`)
 };
 
 const persistCartItems = () => {
@@ -53,19 +54,18 @@ const persistCartItems = () => {
   const currentCart = JSON.parse(localStorage.getItem('cartItems'));
 
   currentCart.forEach((item) => {
-  	let price = parseInt(item.price.split('').slice(1).join(''));
-  	console.log(price)
+    let price = parseInt(item.price.split('').slice(1).join(''));
     
     addToCart(item);
     sumTotal(price);
   });
 
-	const savedTotal = currentCart.reduce((acc, item) => {
-		
-		let itemPrice = parseInt(item.price.split('').slice(1).join(''));
-		
-		return acc + itemPrice
-	}, 0);
+  const savedTotal = currentCart.reduce((acc, item) => {
+    
+    let itemPrice = parseInt(item.price.split('').slice(1).join(''));
+    
+    return acc + itemPrice
+  }, 0);
   
   $('#total').text(`$${savedTotal}`)
 };
@@ -86,78 +86,84 @@ const setLocalStorage = (obj) => {
 };
 
 const processOrder = (total) => {
-	fetch('/api/v1/orderHistory', {
-		method: 'POST',
-		headers: { 'Content-Type':'application/json' },
-		body: JSON.stringify({ total: total })
-	})
-	.then(() => {
-		getOrderHistory()
-	});
+  fetch('/api/v1/orderHistory', {
+    method: 'POST',
+    headers: { 'Content-Type':'application/json' },
+    body: JSON.stringify({ total: total })
+  })
+  .then(response => {
+    if (response.status === 201) {
+      getOrderHistory()
+    } else if (response.status === 422) {
+      alert('There was an problem in processing your order, please try again.')
+    }
+  })
+  .catch(error => console.log(error, 'There was an error processing your order.'))
 };
 
 const getOrderHistory = () => {
-	fetch('/api/v1/orderHistory')
-	.then(response => response.json())
-	.then(orders => {
-		prependOrders(orders)
-	});
+  fetch('/api/v1/orderHistory')
+  .then(response => response.json())
+  .then(orders => {
+    prependOrders(orders)
+  })
+  .catch((error) => {
+    console.log(error, 'There was an error retrieving order history.')
+  });
 };
 
 const prependOrders = (array) => {
-	if (!array.length) {
-		return;
-	}
-	$('#order-card-holder').empty();
-	array.forEach(order => {
-		let cardTotal = `$${order.total}`;
-		let cardDate = order.created_at.slice(0, 10);
+  if (!array.length) {
+    return;
+  }
+  $('#order-card-holder').empty();
+  array.forEach(order => {
+    let cardTotal = `$${order.total}`;
+    let cardDate = order.created_at.slice(0, 10);
 
-		$('#order-card-holder').prepend(
-			`<article class="cart-card">
-				<p class="order-total">Order total: ${cardTotal}</p>
-				<p class="order-date">Order date: ${cardDate}</p>
-			</article>`
-		);
-	});
+    $('#order-card-holder').prepend(
+      `<article class="cart-card">
+        <p class="order-total">Order total: ${cardTotal}</p>
+        <p class="order-date">Order date: ${cardDate}</p>
+      </article>`
+    );
+  });
 };
 
 const clearCart = () => {
-	localStorage.clear();
-	$('#cart-card-holder').empty();
-	$('#total').text('$0.00');
+  localStorage.clear();
+  $('#cart-card-holder').empty();
+  $('#total').text('$0.00');
 }
 
 $('#toggle-order-history').on('click', function() {
-	const orderExpand = $('#toggle-order-history');
+  const orderExpand = $('#toggle-order-history');
 
-	$('#orders').toggleClass('hidden');
-	!orderExpand.text('+') ? orderExpand.text('+') : orderExpand.text('-');
+  $('#orders').toggleClass('hidden');
+  !orderExpand.text('+') ? orderExpand.text('+') : orderExpand.text('-');
 });
 
 $('#toggle-cart').on('click', function() {
-	const orderExpand = $('#toggle-cart');
+  const orderExpand = $('#toggle-cart');
 
-	$('#cart-list').toggleClass('hidden');
-	!orderExpand.text('+') ? orderExpand.text('+') : orderExpand.text('-');
+  $('#cart-list').toggleClass('hidden');
+  !orderExpand.text('+') ? orderExpand.text('+') : orderExpand.text('-');
 });
 
-
-
 $('.inventory-gallery').on('click', '.add-to-cart', function() {
-	let title = this.closest('.inventory-item').children[0].innerText;
-	let price = this.closest('.inventory-item').children[3].innerText;
-	let cartItem = { title, price };
-	let priceInt = parseInt(price.split('').slice(1).join(''));
+  let title = this.closest('.inventory-item').children[0].innerText;
+  let price = this.closest('.inventory-item').children[3].innerText;
+  let cartItem = { title, price };
+  let priceInt = parseInt(price.split('').slice(1).join(''));
 
-	addToCart(cartItem);
-	sumTotal(priceInt);
-	setLocalStorage(cartItem);
+  addToCart(cartItem);
+  sumTotal(priceInt);
+  setLocalStorage(cartItem);
 });
 
 $('.checkout-btn').on('click', function() {
-	let orderTotal = parseInt($('#total').text().slice(1));
-	
-	processOrder(orderTotal);
-	clearCart();
+  let orderTotal = parseInt($('#total').text().slice(1));
+  
+  processOrder(orderTotal);
+  clearCart();
 });
